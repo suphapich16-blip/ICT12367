6143f1197ecea31cf76722bcfe596c62f7c8f8b7
suphapich16-blip <suphapich16@gmail.com>
2026-02-17 11:56:17 +0700

    Add files via upload

---
+++ Lab6/test.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+# ตามด้วยโค้ดเดิมของคุณ
+age = 25
+print("อายุของฉันคือ", age)
+
+name = "Setthapong"
+age = 22
+print(f"สวัสดี {name}, อายุ {age} ปี")
+sep=" | " 
+print("Hello") 
+print("Python", "Java", "C++", sep=" | ") 
